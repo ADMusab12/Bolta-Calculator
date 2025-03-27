b7b77658a079ba1c85d0f2db5e11ca70a389aa89
ADMusab12 <sheikhmusab456@gmail.com>
2025-03-27 20:46:53 +0500

    demo upload

--- app/src/main/java/com/example/boltacalculator/Global.kt
+++ app/src/main/java/com/example/boltacalculator/Global.kt
@@ -1,6 +1,6 @@
 package com.example.boltacalculator
 
 object Global {
-    val GEMINI_KEY ="your api key"
+    val GEMINI_KEY ="AIzaSyD4nhriKJGPKeuCHab1oy7zz1hoVexr4Rc"
     var gemini_reply = ""
 }
