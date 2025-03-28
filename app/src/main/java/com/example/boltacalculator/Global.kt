ad40062cbbbd9b77f1b6ac35725682376dc51344
ADMusab12 <sheikhmusab456@gmail.com>
2025-03-28 07:33:58 +0500

    remove unuse code

--- app/src/main/java/com/example/boltacalculator/Global.kt
+++ app/src/main/java/com/example/boltacalculator/Global.kt
@@ -1,6 +1,6 @@
 package com.example.boltacalculator
 
 object Global {
-    val GEMINI_KEY ="AIzaSyD4nhriKJGPKeuCHab1oy7zz1hoVexr4Rc"
+    val GEMINI_KEY = Your api key
     var gemini_reply = ""
 }
